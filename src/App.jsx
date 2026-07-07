93f13e14574a219e0c6ec3bbe9b862ae19b78152
Nattuwut <nattuwut_hunpoo@hotmail.com>
2026-07-07 11:27:10 +0700

    Create project component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import './App.css'
+import Navbar from './components/NavBar';
+import Menu from './components/Menu';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import About from './components/About';
 
 function App() {
-  return <h1>Hello Coffee Shop</h1>;
+  return <div>
+    <Navbar />
+    <Hero />
+    <About />
+    <Menu />
+    <Contact />
+    <Footer />
+  </div>
 }
 
 export default App;
