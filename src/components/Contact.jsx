1870bcc2c781a811bb3d6770bcc8ebf56bdddb68
Nattuwut <nattuwut_hunpoo@hotmail.com>
2026-07-14 11:46:52 +0700

    create Contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,25 @@
+import "./Contact.css"
+
 function Contact() {
-    return <h1>Contact</h1>
+    return (
+        <section className="contact">
+            <h2>Contact Us</h2>
+            <p>Have questions? We'd love to hear from you.</p>
+            <form >
+                <label htmlFor="name">Name</label>
+                <input id="name" type="text" />
+
+                <label htmlFor="email">Email</label>
+                <input id="email" type="email" />
+
+                <label htmlFor="message">Message</label>
+                <textarea name="" id="message"></textarea>
+
+                <button type="submit">Send Message</button>
+            </form>
+
+        </section>
+    )
 }
 
 export default Contact
